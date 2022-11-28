1ee5ed0538ddbc0161fe0ad66d8b53002d02471d
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2022-11-28 07:54:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 combustivel = () => {
 
     const dist = parseFloat(document.getElementById('i1').value)
-    const rend = parseFloat(document.getElementById('i1').value)
-    const price = parseFloat(document.getElementById('i1').value)
+    const rend = parseFloat(document.getElementById('i2').value)
+    const price = parseFloat(document.getElementById('i3').value)
     
-    document.getElementById("answer").value = 'R$'((dist/rend)*price*1.10).toFixed(2);
+    document.getElementById("answer").value = 'R$' + ((dist/rend)*price*1.10).toFixed(2);
 }
-
-
-
